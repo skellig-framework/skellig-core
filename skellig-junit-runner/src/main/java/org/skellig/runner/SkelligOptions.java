8cf298cf904c62cf0cb52fdbcb8211ff44545fdb
Shendor <shendor1988@gmail.com>
2015-12-30 17:59:46 +0000

    Created simple junit runner for Skellig-features

---
+++ skellig-junit-runner/src/main/java/org/skellig/runner/SkelligOptions.java
@@ -0,0 +1,13 @@
+package org.skellig.runner;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface SkelligOptions {
+
+    String[] features() default {};
+}
---
+++ skellig-junit-runner/src/test/java/org/skellig/runner/DefaultSkelligRunner.java
@@ -0,0 +1,12 @@
+package org.skellig.runner;
+
+import org.junit.runner.RunWith;
+
+@RunWith(SkelligRunner.class)
+@SkelligOptions(
+        features = {"/feature"}
+)
+public class DefaultSkelligRunner {
+
+
+}
