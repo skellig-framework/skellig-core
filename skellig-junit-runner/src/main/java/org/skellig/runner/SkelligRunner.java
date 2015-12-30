63f6be38e429cf226a80b8fa70582fd14a191ca6
Shendor <shendor1988@gmail.com>
2015-12-30 17:59:46 +0000

    Created simple junit runner for Skellig-features

---
+++ skellig-junit-runner/src/main/java/org/skellig/runner/SkelligRunner.java
@@ -0,0 +1,71 @@
+package org.skellig.runner;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.Statement;
+import org.skellig.feature.parser.DefaultFeatureParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SkelligRunner extends ParentRunner<FeatureRunner> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SkelligRunner.class);
+
+    private List<FeatureRunner> children;
+
+    public SkelligRunner(Class clazz) throws Exception {
+        super(clazz);
+
+        SkelligOptions skelligOptions = (SkelligOptions) clazz.getDeclaredAnnotation(SkelligOptions.class);
+
+        DefaultFeatureParser featureParser = new DefaultFeatureParser();
+        children = featureParser.parse(Paths.get(getClass().getResource(skelligOptions.features()[0]).toURI()).toString()).stream()
+                .map(FeatureRunner::create)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void run(RunNotifier notifier) {
+        super.run(notifier);
+    }
+
+    @Override
+    public List<FeatureRunner> getChildren() {
+        return children;
+    }
+
+    @Override
+    protected Description describeChild(FeatureRunner child) {
+        return child.getDescription();
+    }
+
+    @Override
+    protected void runChild(FeatureRunner child, RunNotifier notifier) {
+        child.run(notifier);
+    }
+
+    @Override
+    protected Statement childrenInvoker(RunNotifier notifier) {
+        Statement runFeatures = super.childrenInvoker(notifier);
+        return new RunSkellig(runFeatures);
+    }
+
+
+    class RunSkellig extends Statement {
+        private final Statement runFeatures;
+
+        RunSkellig(Statement runFeatures) {
+            this.runFeatures = runFeatures;
+        }
+
+        @Override
+        public void evaluate() throws Throwable {
+            runFeatures.evaluate();
+        }
+    }
+}
---
+++ skellig-junit-runner/src/main/java/org/skellig/runner/exception/FeatureRunnerException.java
@@ -0,0 +1,8 @@
+package org.skellig.runner.exception;
+
+public class FeatureRunnerException extends RuntimeException {
+
+    public FeatureRunnerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
