8262e3e0e74284701af53efa647e43a1920e649a
Shendor <shendor1988@gmail.com>
2016-01-21 20:18:11 +0000

    Improved context and Test Step processor

--- skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/SkelligTestContext.java
+++ skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/SkelligTestContext.java
@@ -2,6 +2,8 @@ package org.skellig.teststep.processing;
 
 import org.skellig.teststep.processing.processor.DefaultTestStepProcessor;
 import org.skellig.teststep.processing.processor.TestStepProcessor;
+import org.skellig.teststep.processing.state.TestScenarioState;
+import org.skellig.teststep.processing.state.ThreadLocalTestScenarioState;
 import org.skellig.teststep.reader.TestStepReader;
 import org.skellig.teststep.reader.sts.StsTestStepReader;
 
@@ -11,11 +13,16 @@ public class SkelligTestContext {
 
     protected TestStepReader testStepReader;
     protected TestStepProcessor testStepProcessor;
+    protected TestScenarioState testScenarioState;
 
     public SkelligTestContext() {
     }
 
-    public void initialize() {
+    public void initialize(ClassLoader classLoader) {
+        if(testScenarioState == null){
+            testScenarioState = new ThreadLocalTestScenarioState();
+        }
+
         if (testStepReader == null) {
             testStepReader = new StsTestStepReader.Builder()
                     .withDefaultTestStepFactory(getTestStepKeywordsProperties())
@@ -23,7 +30,10 @@ public class SkelligTestContext {
         }
 
         if(testStepProcessor == null){
-            testStepProcessor = new DefaultTestStepProcessor();
+            testStepProcessor = new DefaultTestStepProcessor.Builder()
+                    .withClassLoader(classLoader)
+                    .withTestScenarioState(testScenarioState)
+                    .build();
         }
     }
 
--- skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/processor/DefaultTestStepProcessor.java
+++ skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/processor/DefaultTestStepProcessor.java
@@ -1,14 +1,85 @@
 package org.skellig.teststep.processing.processor;
 
+import org.skellig.teststep.processing.converter.DateValueConverter;
+import org.skellig.teststep.processing.converter.FileValueConverter;
+import org.skellig.teststep.processing.converter.IncrementValueConverter;
+import org.skellig.teststep.processing.converter.TestStepStateValueConverter;
+import org.skellig.teststep.processing.converter.TestStepValueConverter;
+import org.skellig.teststep.processing.converter.ValueExtractor;
+import org.skellig.teststep.processing.state.TestScenarioState;
 import org.skellig.teststep.reader.model.TestStep;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class DefaultTestStepProcessor implements TestStepProcessor {
+
+    private List<TestStepValueConverter> valueConverters;
+    private List<TestStepProcessor> testStepProcessors;
+
+    protected DefaultTestStepProcessor(List<TestStepValueConverter> valueConverters,
+                                       List<TestStepProcessor> testStepProcessors) {
+        this.valueConverters = valueConverters;
+        this.testStepProcessors = testStepProcessors;
+    }
 
     @Override
     public void process(TestStep testStep, Map<String, String> parameters) {
         //TODO: apply parameters and run pre-processing
 
+    }
+
+    public static class Builder {
+
+        private TestStepStateValueConverter.Builder builderForStateValueConverter;
+        private List<TestStepValueConverter> valueConverters;
+        private List<TestStepProcessor> testStepProcessors;
+        private TestScenarioState testScenarioState;
+        private ClassLoader classLoader;
+
+        public Builder() {
+            valueConverters = new ArrayList<>();
+            testStepProcessors = new ArrayList<>();
+            builderForStateValueConverter = new TestStepStateValueConverter.Builder();
+        }
+
+        public Builder withTestStepProcessors(TestStepProcessor testStepProcessor) {
+            this.testStepProcessors.add(testStepProcessor);
+            return this;
+        }
+
+        public Builder withValueConverter(TestStepValueConverter valueConverter) {
+            this.valueConverters.add(valueConverter);
+            return this;
+        }
+
+        public Builder withValueOfStateExtractor(ValueExtractor valueExtractor) {
+            builderForStateValueConverter.withValueExtractor(valueExtractor);
+            return this;
+        }
+
+        public Builder withTestScenarioState(TestScenarioState testScenarioState) {
+            this.testScenarioState = testScenarioState;
+            return this;
+        }
+
+        public Builder withClassLoader(ClassLoader classLoader) {
+            this.classLoader = classLoader;
+            return this;
+        }
+
+        public TestStepProcessor build() {
+            Objects.requireNonNull(classLoader, "ClassLoader must be provided");
+            Objects.requireNonNull(testScenarioState, "TestScenarioState must be provided");
+
+            valueConverters.add(builderForStateValueConverter.build(testScenarioState));
+            valueConverters.add(new DateValueConverter());
+            valueConverters.add(new FileValueConverter(classLoader));
+            valueConverters.add(new IncrementValueConverter());
+
+            return new DefaultTestStepProcessor(valueConverters, testStepProcessors);
+        }
     }
 }
