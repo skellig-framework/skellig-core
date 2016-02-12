a9e71410b08f05da32fbc025f2d8e154a65cd1e7
Shendor <shendor1988@gmail.com>
2016-02-12 21:29:16 +0000

    Removed unused regex in ValidationType. Add validation type in report of validation section.

--- skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/model/ValidationType.java
+++ skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/model/ValidationType.java
@@ -1,28 +1,8 @@
 package org.skellig.teststep.processing.model;
 
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
-
 public enum ValidationType {
-    ALL_MATCH(Pattern.compile("all[\\s_]?match")),
-    NONE_MATCH(Pattern.compile("none[\\s_]?match")),
-    ANY_MATCH(Pattern.compile("any[\\s_]?match")),
-    ANY_NONE_MATCH(Pattern.compile("any[\\s_]/none[\\s_]?match"));
-
-    private Pattern pattern;
-
-    ValidationType(Pattern pattern) {
-        this.pattern = pattern;
-    }
-
-    public static ValidationType getValidationTypeFor(String actualValue) {
-        return Stream.of(values())
-                .filter(validationType -> validationType.pattern.matcher(actualValue).matches())
-                .findFirst()
-                .orElse(ValidationType.ALL_MATCH);
-    }
-
-    public Pattern getPattern() {
-        return pattern;
-    }
+    ALL_MATCH,
+    NONE_MATCH,
+    ANY_MATCH,
+    ANY_NONE_MATCH;
 }
