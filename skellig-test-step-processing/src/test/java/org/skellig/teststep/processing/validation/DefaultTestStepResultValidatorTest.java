2ee0d7019515db2995ec5796f36c2513501c20e8
Shendor <shendor1988@gmail.com>
2016-02-12 19:50:53 -0800

    Improved validation details factory to support grouped properties

--- skellig-test-step-processing/src/test/java/org/skellig/teststep/processing/validation/DefaultTestStepResultValidatorTest.java
+++ skellig-test-step-processing/src/test/java/org/skellig/teststep/processing/validation/DefaultTestStepResultValidatorTest.java
@@ -1,6 +1,7 @@
 package org.skellig.teststep.processing.validation;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.skellig.teststep.processing.exception.ValidationException;
 import org.skellig.teststep.processing.model.ExpectedResult;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.skellig.teststep.processing.utils.UnitTestUtils.createMap;
 
+@DisplayName("Validate result")
 class DefaultTestStepResultValidatorTest {
 
     private TestStepResultValidator validator;
@@ -32,6 +34,7 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When actual Map fully matches expected Map Then pass validation")
     void testValidateWhenValid() {
         Map<String, Object> actualResult = createActualResult();
 
@@ -46,6 +49,7 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When actual String And expect few contains values Then pass validation")
     void testValidateListOfContainsTextWhenValid() {
         String actualResult = "v1 v2";
 
@@ -59,8 +63,9 @@ class DefaultTestStepResultValidatorTest {
         validator.validate(expectedResult, actualResult);
     }
 
-//    @Test
-    void testValidateListOfContainsTextWhenActualListOfTextsAndNotValid() {
+    @Test
+    @DisplayName("When expected contains+size under single group And not match actual List of String")
+    void testValidateListOfContainsTextUnderGroupAndNotMatchWithActualResult() {
         List<String> actualResult = new ArrayList<>(Arrays.asList("v1 v2", "v3 v4"));
 
         ExpectedResult expectedResult =
@@ -93,6 +98,7 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When actual Map doesn't match expected Map")
     void testValidateWhenNotValid() {
         Map<String, Object> actualResult = createActualResult();
 
@@ -110,6 +116,7 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When any match with actual Map Then pass validation")
     void testValidateAnyMatch() {
         Map<String, Object> actualResult = createActualResult();
 
@@ -124,7 +131,8 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
-    void testValidateWithArrayOfExpectedResults() {
+    @DisplayName("When expected List of any Map not under single group And match at least one Then pass validation")
+    void testValidateWithAnyMatchArrayOfMapNotUnderGroup() {
         Map<String, Object> actualResult = createActualResult();
 
         ExpectedResult expectedResult =
@@ -148,8 +156,9 @@ class DefaultTestStepResultValidatorTest {
         validator.validate(expectedResult, Collections.singletonList(actualResult));
     }
 
-//    @Test
-    void testValidateWhenActualResultIsListOfMap() {
+    @Test
+    @DisplayName("When actual and expected is List of Map And all match And under single group Then pass validation")
+    void testValidateAllMatchListOfMapWithListOfMapUnderSingleGroup() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -180,7 +189,8 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
-    void testValidateWhenActualResultIsListOfMap2() {
+    @DisplayName("When actual and expected is List of Map And all match And not under single group Then pass validation")
+    void testValidateAllMatchListOfMapWithListOfMapWithoutSingleGroup() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -205,8 +215,9 @@ class DefaultTestStepResultValidatorTest {
         validator.validate(expectedResult, new ArrayList<>(Arrays.asList(actualResult1, actualResult2)));
     }
 
-//    @Test
-    void testValidateWhenActualResultIsListOfMapWhenNoMatch() {
+    @Test
+    @DisplayName("When actual and expected is List of Map And under single group with none match Then pass validation")
+    void testValidateAllMatchListOfMapWithListOfMapUnderSingleNoneMatchGroup() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -237,7 +248,8 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
-    void testValidateWhenActualResultIsListOfMapAndCheckNoMatch() {
+    @DisplayName("When none match expected And actual is array Then pass validation")
+    void testValidateNoneMatchWhenActualNotHaveExpected() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -259,7 +271,8 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
-    void testValidateWhenActualResultIsListOfMapAndCheckNoMatchWhenNotValid() {
+    @DisplayName("When none match expected And actual has expected data Then fail validation")
+    void testValidateNoneMatchWhenActualHasExpected() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -285,7 +298,8 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
-    void testValidateWhenActualResultIsListOfMapWhenNoneMatch() {
+    @DisplayName("When none match expected in root group And actual has expected data Then fail validation")
+    void testValidateNoneMatchInRootGroupAndActualHasExpected() {
         Map<String, Object> actualResult1 = createActualResult();
         Map<String, Object> actualResult2 = createAnotherActualResult();
 
@@ -307,11 +321,12 @@ class DefaultTestStepResultValidatorTest {
                         ),
                         ValidationType.NONE_MATCH);
 
-        ValidationException e = assertThrows(ValidationException.class,
+        assertThrows(ValidationException.class,
                 () -> validator.validate(expectedResult, Arrays.asList(actualResult1, actualResult2)));
     }
 
     @Test
+    @DisplayName("When any match expected And actual doesn't have expected Then fail validation")
     void testValidateAnyMatchWhenNotValid() {
         Map<String, Object> actualResult = createActualResult();
 
@@ -331,6 +346,7 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When none match expected And actual doesn't have expected Then fail validation")
     void testValidateNoneMatchWhenNotValid() {
         Map<String, Object> actualResult = createActualResult();
 
@@ -350,14 +366,16 @@ class DefaultTestStepResultValidatorTest {
     }
 
     @Test
+    @DisplayName("When actual value is complex And not match Then fail validation")
     void testValidateComplexValueWhenNotValid() {
         Map<String, Object> actualResult = createMap("k1", createMap("k2", "v2"));
 
         ExpectedResult expectedResult =
                 new ExpectedResult("",
-                        Collections.singletonList(new ExpectedResult("k1",
-                                Collections.singletonList(new ExpectedResult("k2", "v3", null)),
-                                ValidationType.ALL_MATCH)),
+                        Collections.singletonList(
+                                new ExpectedResult("k1",
+                                        Collections.singletonList(new ExpectedResult("k2", "v3", null)),
+                                        ValidationType.ALL_MATCH)),
                         ValidationType.ALL_MATCH);
 
         new ValidationDetails.Builder().withExpectedResult(expectedResult).build();
