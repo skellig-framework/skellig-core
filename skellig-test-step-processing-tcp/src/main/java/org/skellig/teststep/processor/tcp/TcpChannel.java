9db62e01b6f149c2ec21a9524254f9ce38c27bdb
Shendor <shendor1988@gmail.com>
2016-02-14 22:03:29 +0000

    Added TCP processor and moved tcp channel there

---
+++ skellig-test-step-processing-tcp/src/main/java/org/skellig/teststep/processor/tcp/TcpChannel.java
@@ -0,0 +1,93 @@
+package org.skellig.teststep.processor.tcp;
+
+import org.skellig.connection.channel.exception.ChannelConnectionException;
+import org.skellig.teststep.processor.tcp.model.TcpDetails;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Optional;
+
+class TcpChannel implements AutoCloseable {
+
+    private Socket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private boolean isClosed;
+
+    TcpChannel(TcpDetails tcpDetails) throws ChannelConnectionException {
+        try {
+            socket = new Socket();
+            socket.setKeepAlive(true);
+            socket.setSoTimeout(0);
+            socket.connect(new InetSocketAddress(InetAddress.getByName(tcpDetails.getHostName()), tcpDetails.getPort()));
+
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new ChannelConnectionException(e);
+        }
+    }
+
+    public void send(Object request) {
+        if (socket.isConnected()) {
+            try {
+                byte[] messageAsBytes = (byte[]) request;
+                outputStream.write(messageAsBytes, 0, messageAsBytes.length);
+                outputStream.flush();
+            } catch (Exception e) {
+                // log later
+            }
+        }
+    }
+
+    public Optional<Object> read() {
+        try {
+            Optional<Object> response;
+            do {
+                response = readIfAvailable();
+                Thread.sleep(5);
+            } while (!response.isPresent() && !isClosed);
+
+            return response;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<Object> readIfAvailable() {
+        try {
+            if (inputStream.available() > 0) {
+                byte[] bytes = new byte[inputStream.available()];
+                inputStream.readFully(bytes, 0, bytes.length);
+                return Optional.of(bytes);
+            }
+        } catch (Exception e) {
+            //log later
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public synchronized void close() {
+        try {
+            if (socket != null && socket.isConnected()) {
+                if (!socket.isClosed()) {
+                    inputStream.close();
+                    outputStream.close();
+                }
+                socket.close();
+            }
+            isClosed = true;
+        } catch (Exception e) {
+            //log later
+        }
+    }
+
+    boolean isClosed() {
+        return isClosed;
+    }
+}
