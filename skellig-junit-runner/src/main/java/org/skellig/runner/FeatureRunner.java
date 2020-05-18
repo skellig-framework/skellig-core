17e309b89b6006c8a28c1c961f4a8f7f0c22ea2f
skellig-dev <Roman.Damaschin@mastercard.com>
2020-05-18 17:56:36 +0100

    Improved feature runner

--- skellig-junit-runner/src/main/java/org/skellig/runner/FeatureRunner.java
+++ skellig-junit-runner/src/main/java/org/skellig/runner/FeatureRunner.java
@@ -1,33 +1,29 @@
 package org.skellig.runner;
 
 import org.junit.runner.Description;
-import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.ParentRunner;
 import org.junit.runners.model.InitializationError;
 import org.skellig.feature.Feature;
-import org.skellig.feature.TestScenario;
-import org.skellig.feature.TestStep;
 import org.skellig.runner.exception.FeatureRunnerException;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FeatureRunner extends ParentRunner<TestScenarioRunner> {
 
-    private List<TestScenarioRunner> testScenarioRunners;
     private Feature feature;
+    private List<TestScenarioRunner> testScenarioRunners;
     private Description description;
 
-    public FeatureRunner(Feature feature) throws InitializationError {
+    protected FeatureRunner(Feature feature) throws InitializationError {
         super(feature.getClass());
-        testScenarioRunners = feature.getScenarios().stream()
-                .map(TestScenarioRunner::create)
-                .collect(Collectors.toList());
         this.feature = feature;
+        testScenarioRunners =
+                feature.getScenarios().stream()
+                        .map(TestScenarioRunner::create)
+                        .collect(Collectors.toList());
     }
 
     @Override
@@ -55,20 +51,16 @@ public class FeatureRunner extends ParentRunner<TestScenarioRunner> {
     }
 
     @Override
-    public void run(RunNotifier notifier) {
-        super.run(notifier);
-    }
-
-    @Override
     protected void runChild(TestScenarioRunner child, RunNotifier notifier) {
-        notifier.fireTestStarted(describeChild(child));
+        Description childDescription = describeChild(child);
+        notifier.fireTestStarted(childDescription);
         try {
             child.run(notifier);
         } catch (Throwable e) {
-            notifier.fireTestFailure(new Failure(describeChild(child), e));
+            notifier.fireTestFailure(new Failure(childDescription, e));
             notifier.pleaseStop();
         } finally {
-            notifier.fireTestFinished(describeChild(child));
+            notifier.fireTestFinished(childDescription);
         }
     }
 
@@ -81,64 +73,3 @@ public class FeatureRunner extends ParentRunner<TestScenarioRunner> {
     }
 }
 
-class TestScenarioRunner extends ParentRunner<TestStep> {
-
-    private final Map<TestStep, Description> stepDescriptions = new HashMap<>();
-    private Description description;
-    private TestScenario testScenario;
-
-    private TestScenarioRunner(TestScenario testScenario) throws InitializationError {
-        super(testScenario.getClass());
-        this.testScenario = testScenario;
-    }
-
-    @Override
-    protected List<TestStep> getChildren() {
-        return testScenario.getSteps();
-    }
-
-    @Override
-    protected String getName() {
-        return testScenario.getName();
-    }
-
-    @Override
-    public Description getDescription() {
-        if (description == null) {
-            description = Description.createSuiteDescription(getName(), testScenario.getName());
-            getChildren().forEach(step -> description.addChild(describeChild(step)));
-        }
-        return description;
-    }
-
-    @Override
-    public Description describeChild(TestStep step) {
-        Description description = stepDescriptions.get(step);
-        if (description == null) {
-            description = Description.createTestDescription(getName(), step.getName(), step.getName());
-            stepDescriptions.put(step, description);
-        }
-        return description;
-    }
-
-    @Override
-    protected void runChild(TestStep child, RunNotifier notifier) {
-        notifier.fireTestStarted(describeChild(child));
-        try {
-            //TODO: run test case from test data
-        } catch (Throwable e) {
-            notifier.fireTestFailure(new Failure(describeChild(child), e));
-            notifier.pleaseStop();
-        } finally {
-            notifier.fireTestFinished(describeChild(child));
-        }
-    }
-
-    public static TestScenarioRunner create(TestScenario testScenario) {
-        try {
-            return new TestScenarioRunner(testScenario);
-        } catch (InitializationError e) {
-            throw new FeatureRunnerException(e.getMessage(), e);
-        }
-    }
-}
---
+++ skellig-junit-runner/src/main/java/org/skellig/runner/TestScenarioRunner.java
@@ -0,0 +1,73 @@
+package org.skellig.runner;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.InitializationError;
+import org.skellig.feature.TestScenario;
+import org.skellig.feature.TestStep;
+import org.skellig.runner.exception.FeatureRunnerException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestScenarioRunner extends ParentRunner<TestStep> {
+
+    private final Map<Object, Description> stepDescriptions = new HashMap<>();
+    private TestScenario testScenario;
+
+    protected TestScenarioRunner(TestScenario testScenario) throws InitializationError {
+        super(testScenario.getClass());
+        this.testScenario = testScenario;
+    }
+
+    @Override
+    protected List<TestStep> getChildren() {
+        return testScenario.getSteps();
+    }
+
+    @Override
+    protected String getName() {
+        return testScenario.getName();
+    }
+
+    @Override
+    public Description getDescription() {
+        return stepDescriptions.computeIfAbsent(this,
+                o -> {
+                    Description description = Description.createSuiteDescription(getName(), testScenario.getName());
+                    getChildren().forEach(step -> description.addChild(describeChild(step)));
+                    return description;
+                });
+    }
+
+    @Override
+    public Description describeChild(TestStep step) {
+        return stepDescriptions.getOrDefault(step,
+                Description.createTestDescription(getName(), step.getName(), step.getName()));
+    }
+
+    @Override
+    protected void runChild(TestStep child, RunNotifier notifier) {
+        Description childDescription = describeChild(child);
+        notifier.fireTestStarted(childDescription);
+        try {
+            //TODO: run test case from test data
+        } catch (Throwable e) {
+            notifier.fireTestFailure(new Failure(childDescription, e));
+            notifier.pleaseStop();
+        } finally {
+            notifier.fireTestFinished(childDescription);
+        }
+    }
+
+    public static TestScenarioRunner create(TestScenario testScenario) {
+        try {
+            return new TestScenarioRunner(testScenario);
+        } catch (InitializationError e) {
+            throw new FeatureRunnerException(e.getMessage(), e);
+        }
+    }
+}
