80682d58ef0de5b1b77c6b98ac896c59465b7705
skellig-dev <Roman.Damaschin@mastercard.com>
2020-06-14 20:12:45 +0100

    Improved validation details factory to support grouped properties

--- skellig-test-step-reader/src/test/java/org/skellig/teststep/reader/model/factory/ValidationDetailsFactoryTest.java
+++ skellig-test-step-reader/src/test/java/org/skellig/teststep/reader/model/factory/ValidationDetailsFactoryTest.java
@@ -32,8 +32,94 @@ class ValidationDetailsFactoryTest {
     }
 
     @Test
+    @DisplayName("When has few expected values")
+    void testSimpleExpectedResult() {
+        Map<String, Object> rawValidationDetails =
+                createMap("status", "200", "log", "");
+
+        TestStep testStep = validationDetailsFactory.create(createMap("validate", rawValidationDetails));
+        ValidationDetails validationDetails = testStep.getValidationDetails().get();
+
+        assertAll(
+                () -> assertEquals(ValidationType.ALL_MATCH, validationDetails.getExpectedResult().getValidationType()),
+                () -> assertEquals("log", extractExpectedValue(validationDetails.getExpectedResult(), 0).getProperty()),
+                () -> assertEquals("", extractExpectedValue(validationDetails.getExpectedResult(), 0).getExpectedResult()),
+
+                () -> assertEquals("status", extractExpectedValue(validationDetails.getExpectedResult(), 1).getProperty()),
+                () -> assertEquals("200", extractExpectedValue(validationDetails.getExpectedResult(), 1).getExpectedResult())
+
+        );
+    }
+
+    @Test
+    @DisplayName("When has array of expected values as Map ")
+    void testWithArrayOfMaps() {
+        Map<String, Object> rawValidationDetails =
+                createMap("all_match",
+                        Arrays.asList(
+                                createMap("a1", "v1", "a2", "v2"),
+                                createMap("b1", "v1", "b2", "v2")
+                        ));
+
+        TestStep testStep = validationDetailsFactory.create(createMap("validate", rawValidationDetails));
+        ValidationDetails validationDetails = testStep.getValidationDetails().get();
+
+        assertAll(
+                () -> assertEquals(ValidationType.ALL_MATCH, validationDetails.getExpectedResult().getValidationType()),
+                () -> assertEquals("", validationDetails.getExpectedResult().getProperty()),
+
+                () -> assertNull(extractExpectedValue(validationDetails.getExpectedResult(), 0).getProperty()),
+                () -> assertEquals("a1", extractExpectedValue(validationDetails.getExpectedResult(), 0, 0).getProperty()),
+                () -> assertEquals("v1", extractExpectedValue(validationDetails.getExpectedResult(), 0, 0).getExpectedResult()),
+                () -> assertEquals("a2", extractExpectedValue(validationDetails.getExpectedResult(), 0, 1).getProperty()),
+                () -> assertEquals("v2", extractExpectedValue(validationDetails.getExpectedResult(), 0, 1).getExpectedResult()),
+
+                () -> assertNull(extractExpectedValue(validationDetails.getExpectedResult(), 1).getProperty()),
+                () -> assertEquals("b2", extractExpectedValue(validationDetails.getExpectedResult(), 1, 0).getProperty()),
+                () -> assertEquals("v2", extractExpectedValue(validationDetails.getExpectedResult(), 1, 0).getExpectedResult()),
+                () -> assertEquals("b1", extractExpectedValue(validationDetails.getExpectedResult(), 1, 1).getProperty()),
+                () -> assertEquals("v1", extractExpectedValue(validationDetails.getExpectedResult(), 1, 1).getExpectedResult())
+
+        );
+    }
+
+    /*
+    * This test is to check the following structure from test step file:
+    *
+    * any_match {
+
+      srv1 {
+         status = 200
+         body {
+            json_path(f1.f2) = v1
+            json_path(f1.f3) = v2
+            regex(.*f3=(\\w+).*) = v3
+         }
+         headers {
+            content-type = application/json
+         }
+         log {
+            none_match [
+              contains(fail)
+              contains(error)
+            ]
+         }
+      }
+
+      srv2 {
+         body {
+           json_path(f1.f2) = v1
+         }
+         headers {
+            content-type = application/json
+         }
+         log = contains(success)
+      }
+    }
+    * */
+    @Test
     @DisplayName("When has test id and complex expected results with any_match and none_match")
-    void test() {
+    void testComplexExpectedResult() {
         Map<String, Object> rawValidationDetails = new LinkedHashMap<>();
         rawValidationDetails.put("from_test", "t1");
         rawValidationDetails.put("any_match",
@@ -107,8 +193,50 @@ class ValidationDetailsFactoryTest {
                 () -> assertEquals("json_path(f1.f2)", extractExpectedValue(validationDetails.getExpectedResult(), 1, 2, 0).getProperty()),
                 () -> assertEquals("v1", extractExpectedValue(validationDetails.getExpectedResult(), 1, 2, 0).getExpectedResult()),
 
-                () -> assertEquals("status", extractExpectedValue(validationDetails.getExpectedResult(), 1, 3).getProperty()),
+
                 () -> assertEquals("200", extractExpectedValue(validationDetails.getExpectedResult(), 1, 3).getExpectedResult())
+        );
+    }
+
+    /*
+    * This test is to check the following structure from test step file:
+    *
+    * any_match {
+       [srv1, srv2, srv3] {
+           status = 200
+       }
+       log = contains(success)
+      }
+    * */
+    @Test
+    @DisplayName("When has grouped properties Then verify these properties are split")
+    void testWithGroupedProperties() {
+        Map<String, Object> rawValidationDetails =
+                createMap("any_match",
+                        createMap("[srv1,srv2, srv3]", createMap("status", "200"),
+                                "log", "contains(success)")
+                );
+
+        TestStep testStep = validationDetailsFactory.create(createMap("validate", rawValidationDetails));
+        ValidationDetails validationDetails = testStep.getValidationDetails().get();
+
+        assertAll(
+                () -> assertEquals(ValidationType.ANY_MATCH, validationDetails.getExpectedResult().getValidationType()),
+                () -> assertEquals("log", extractExpectedValue(validationDetails.getExpectedResult(), 0).getProperty()),
+                () -> assertEquals("contains(success)", extractExpectedValue(validationDetails.getExpectedResult(), 0).getExpectedResult()),
+
+                () -> assertEquals("srv3", extractExpectedValue(validationDetails.getExpectedResult(), 1).getProperty()),
+                () -> assertEquals("status", extractExpectedValue(validationDetails.getExpectedResult(), 1, 0).getProperty()),
+                () -> assertEquals("200", extractExpectedValue(validationDetails.getExpectedResult(), 1, 0).getExpectedResult()),
+
+                () -> assertEquals("srv1", extractExpectedValue(validationDetails.getExpectedResult(), 2).getProperty()),
+                () -> assertEquals("status", extractExpectedValue(validationDetails.getExpectedResult(), 2, 0).getProperty()),
+                () -> assertEquals("200", extractExpectedValue(validationDetails.getExpectedResult(), 2, 0).getExpectedResult()),
+
+                () -> assertEquals("srv2", extractExpectedValue(validationDetails.getExpectedResult(), 3).getProperty()),
+                () -> assertEquals("status", extractExpectedValue(validationDetails.getExpectedResult(), 3, 0).getProperty()),
+                () -> assertEquals("200", extractExpectedValue(validationDetails.getExpectedResult(), 3, 0).getExpectedResult())
+
         );
     }
 
