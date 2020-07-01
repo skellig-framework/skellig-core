81a1532b4d412c03b98182c2ab7202ffd7a130cf
skellig-dev <Roman.Damaschin@mastercard.com>
2020-07-01 20:38:27 +0100

    Added TCP channel

--- skellig-connection-channel/src/main/java/org/skellig/connection/channel/factory/SendingChannelFactory.java
+++ skellig-connection-channel/src/main/java/org/skellig/connection/channel/factory/SendingChannelFactory.java
@@ -1,16 +1,12 @@
 package org.skellig.connection.channel.factory;
 
 
-import org.skellig.connection.channel.ReadingChannel;
 import org.skellig.connection.channel.SendingChannel;
 import org.skellig.connection.channel.exception.ChannelConnectionException;
 import org.skellig.connection.channel.model.BaseChannelDetails;
 
-public interface ConnectionChannelFactory {
+public interface SendingChannelFactory {
 
     SendingChannel createSendingChannel(BaseChannelDetails channelDetails)
-            throws ChannelConnectionException;
-
-    ReadingChannel createListeningChannel(BaseChannelDetails channelDetails)
             throws ChannelConnectionException;
 }
