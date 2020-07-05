328fe735529654d3a287e82e8d791798011d5a34
skellig-dev <Roman.Damaschin@mastercard.com>
2020-07-05 16:04:38 +0100

    Added timeout, delay and execution type attributes to TestStep. Refactoring at TestStepFactory

--- skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/model/factory/BaseTestStepFactory.java
+++ skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/model/factory/BaseTestStepFactory.java
@@ -3,7 +3,7 @@ package org.skellig.teststep.processing.model.factory;
 import org.skellig.teststep.processing.converter.TestDataConverter;
 import org.skellig.teststep.processing.converter.TestStepValueConverter;
 import org.skellig.teststep.processing.model.TestStep;
-import org.skellig.teststep.processing.model.ValidationDetails;
+import org.skellig.teststep.processing.model.TestStepExecutionType;
 import org.skellig.teststep.processing.utils.CachedPattern;
 
 import java.util.Arrays;
@@ -24,6 +24,11 @@ public abstract class BaseTestStepFactory implements TestStepFactory {
 
     private static final String TEST_STEP_NAME_KEYWORD = "test.step.keyword.name";
     private static final String VARIABLES_KEYWORD = "test.step.keyword.variables";
+    private static final String EXECUTION_KEYWORD = "test.step.keyword.execution";
+    private static final String TIMEOUT_KEYWORD = "test.step.keyword.timeout";
+    private static final String DELAY_KEYWORD = "test.step.keyword.delay";
+    private static final int DEFAULT_DELAY = 5;
+    private static final int DEFAULT_TIMEOUT = 30;
 
     private static Set<String> testDataKeywords;
 
@@ -68,16 +73,19 @@ public abstract class BaseTestStepFactory implements TestStepFactory {
             additionalParameters.putAll(variables);
         }
 
-        return createTestStep(rawTestStep).create(
-                getId(rawTestStep),
-                testStepName,
-                extractTestData(rawTestStep, additionalParameters),
-                validationDetailsFactory.create(rawTestStep, additionalParameters),
-                additionalParameters,
-                variables);
+        return createTestStepBuilder(rawTestStep, additionalParameters)
+                .withId(getId(rawTestStep))
+                .withName(testStepName)
+                .withTestData(extractTestData(rawTestStep, additionalParameters))
+                .withValidationDetails(validationDetailsFactory.create(rawTestStep, additionalParameters))
+                .withVariables(variables)
+                .withExecution(getExecutionType(rawTestStep))
+                .withTimeout(getTimeout(rawTestStep, additionalParameters))
+                .withDelay(getTimeout(rawTestStep, additionalParameters))
+                .build();
     }
 
-    protected abstract CreateTestStepDelegate createTestStep(Map<String, Object> rawTestStep);
+    protected abstract TestStep.Builder createTestStepBuilder(Map<String, Object> rawTestStep, Map<String, Object> parameters);
 
     protected Collection<String> getStringArrayDataFromRawTestStep(String propertyName, Map<String, Object> rawTestStep,
                                                                    Map<String, Object> parameters) {
@@ -141,6 +149,22 @@ public abstract class BaseTestStepFactory implements TestStepFactory {
         return rawTestStep.containsKey("id") ? String.valueOf(rawTestStep.get("id")) : null;
     }
 
+    protected TestStepExecutionType getExecutionType(Map<String, Object> rawTestStep) {
+        final String executionKeyword = getKeywordName(EXECUTION_KEYWORD, "execution");
+        if (rawTestStep.containsKey(executionKeyword)) {
+            return TestStepExecutionType.fromName(String.valueOf(rawTestStep.get(executionKeyword)));
+        }
+        return TestStepExecutionType.SYNC;
+    }
+
+    protected int getTimeout(Map<String, Object> rawTestStep, Map<String, Object> parameters) {
+        return getInteger(rawTestStep, parameters, getKeywordName(TIMEOUT_KEYWORD, "timeout"), DEFAULT_TIMEOUT);
+    }
+
+    protected int getDelay(Map<String, Object> rawTestStep, Map<String, Object> parameters) {
+        return getInteger(rawTestStep, parameters, getKeywordName(DELAY_KEYWORD, "delay"), DEFAULT_DELAY);
+    }
+
     protected String getName(Map<String, Object> rawTestStep) {
         return String.valueOf(rawTestStep.get(getKeywordName(TEST_STEP_NAME_KEYWORD, "name")));
     }
@@ -153,10 +177,16 @@ public abstract class BaseTestStepFactory implements TestStepFactory {
         return testStepFactoryValueConverter.convertValue(value, parameters);
     }
 
-    protected interface CreateTestStepDelegate {
-
-        TestStep create(String id, String name, Object testData, ValidationDetails validationDetails,
-                        Map<String, Object> parameters, Map<String, Object> variables);
+    private int getInteger(Map<String, Object> rawTestStep, Map<String, Object> parameters,
+                           String keywordName, int defaultValue) {
+        int value = defaultValue;
+        if (rawTestStep.containsKey(keywordName)) {
+            Object rawTimeout = convertValue(rawTestStep.get(keywordName), parameters);
+            try {
+                value = Integer.parseInt(String.valueOf(rawTimeout));
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        return value;
     }
-
 }
