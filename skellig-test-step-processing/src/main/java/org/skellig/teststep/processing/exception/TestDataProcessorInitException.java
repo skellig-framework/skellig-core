d132c0c2baf06cd28bc11f5e2c8087da4f8a875b
skellig-dev <Roman.Damaschin@mastercard.com>
2020-12-08 20:35:26 +0000

    Additional unit tests for DatabaseRequestExecutorFactory

---
+++ skellig-test-step-processing/src/main/java/org/skellig/teststep/processing/exception/TestDataProcessorInitException.java
@@ -0,0 +1,12 @@
+package org.skellig.teststep.processing.exception;
+
+public class TestDataProcessorInitException extends RuntimeException {
+
+    public TestDataProcessorInitException(String message) {
+        super(message);
+    }
+
+    public TestDataProcessorInitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
