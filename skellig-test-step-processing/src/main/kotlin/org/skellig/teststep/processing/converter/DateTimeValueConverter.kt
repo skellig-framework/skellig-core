31dc74125f48d588b18ebf1f2ab27beff712273a
skellig-dev <Roman.Damaschin@mastercard.com>
2021-01-05 20:14:18 +0000

    Added format option to DateTimeValueConverter

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/DateTimeValueConverter.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/DateTimeValueConverter.kt
@@ -1,35 +1,49 @@
 package org.skellig.teststep.processing.converter
 
 import org.apache.commons.lang3.StringUtils
+import org.skellig.teststep.processing.exception.TestDataConversionException
 import java.time.DateTimeException
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.regex.Pattern
 
 class DateTimeValueConverter : TestStepValueConverter {
 
     companion object {
-        private val NOW_REGEX = Pattern.compile("now\\(([\\w]*)\\)")
+        private val NOW_PATTERN = Pattern.compile("now\\(([\\w]*)\\)(.format\\(([\\w-'/:.]*)\\))?")
     }
 
     override fun convert(value: String?): Any? {
-        val matcher = NOW_REGEX.matcher(value ?: "")
-        if (matcher.find()) {
-            val timezone = matcher.group(1)
-            return getLocalDateTime(timezone)
+        return value?.let {
+            val matcher = NOW_PATTERN.matcher(value)
+            if (matcher.find()) {
+                val timezone = matcher.group(1)
+                val format = matcher.group(3)
+                return if (format == null) getLocalDateTime(timezone)
+                else it.replace(matcher.group(0), getLocalDateTime(format, timezone))
+            }
         }
-        return value
     }
 
-    private fun getLocalDateTime(timezone: String): Any? {
+    private fun getLocalDateTime(timezone: String): LocalDateTime {
         return if (StringUtils.isEmpty(timezone)) {
             LocalDateTime.now()
         } else {
             try {
                 LocalDateTime.now(ZoneId.of(timezone))
             } catch (ex: DateTimeException) {
-                LocalDateTime.now()
+                throw TestDataConversionException("Cannot get current date for the timezone '$timezone'")
             }
+        }
+    }
+
+    private fun getLocalDateTime(format: String, timezone: String): String {
+        val now = getLocalDateTime(timezone)
+        return try {
+            now.format(DateTimeFormatter.ofPattern(format))
+        } catch (ex: Exception) {
+            throw TestDataConversionException("Cannot format current date with the format '$format'")
         }
     }
 
