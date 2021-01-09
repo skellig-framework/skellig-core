8baa5aa27a10ae4d43275e39650334372484258e
skellig-dev <Roman.Damaschin@mastercard.com>
2021-01-09 19:22:52 +0000

    Refactoring at NumberValueConverter

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/NumberValueConverter.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/NumberValueConverter.kt
@@ -1,36 +1,47 @@
 package org.skellig.teststep.processing.converter
 
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 class NumberValueConverter : TestStepValueConverter {
 
     companion object {
-        private val INTEGER_REGEX = Pattern.compile("^\\((int|long|short)\\) *(\\d+)$")
-        private val FLOAT_REGEX = Pattern.compile("^\\((float|double)\\) *(\\d+\\.\\d+)$")
+        private val INTEGER_REGEX = Pattern.compile("^(int|long)\\((\\d+)\\)$")
+        private val FLOAT_REGEX = Pattern.compile("^(float|double)\\((\\d+\\.\\d+)\\)$")
     }
 
     override fun convert(value: String?): Any? {
-        val intMatcher = INTEGER_REGEX.matcher(value)
-        if (intMatcher.find()) {
-            val type = intMatcher.group(1)
-            val intValue = intMatcher.group(2)
-            return if (type == "int") {
-                intValue.toInt()
+        return value?.let {
+            val intMatcher = INTEGER_REGEX.matcher(value)
+            if (intMatcher.find()) {
+                return toIntOrLong(intMatcher)
             } else {
-                intValue.toLong()
-            }
-        } else {
-            val floatMatcher = FLOAT_REGEX.matcher(value)
-            if (floatMatcher.find()) {
-                val type = floatMatcher.group(1)
-                val floatValue = floatMatcher.group(2)
-                return if (type == "double") {
-                    floatValue.toDouble()
-                } else {
-                    floatValue.toFloat()
+                val floatMatcher = FLOAT_REGEX.matcher(value)
+                if (floatMatcher.find()) {
+                    return toFloatOrDouble(floatMatcher)
                 }
             }
+            return value
+        } ?: value
+    }
+
+    private fun toFloatOrDouble(floatMatcher: Matcher): Number {
+        val type = floatMatcher.group(1)
+        val floatValue = floatMatcher.group(2)
+        return if (type == "double") {
+            floatValue.toDouble()
+        } else {
+            floatValue.toFloat()
         }
-        return value
+    }
+
+    private fun toIntOrLong(intMatcher: Matcher): Number {
+        val type = intMatcher.group(1)
+        val intValue = intMatcher.group(2)
+        return if (type == "int") {
+            intValue.toInt()
+        } else {
+            intValue.toLong()
+        }
     }
 }
