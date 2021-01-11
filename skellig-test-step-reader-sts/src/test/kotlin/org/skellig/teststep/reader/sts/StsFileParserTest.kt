c1643e9b917caaa5ed2899c35fc3649c75f6196c
Shendor <shendor1988@gmail.com>
2021-01-11 18:15:08 +0000

    Refactoring at StsFileParserTest.kt

--- skellig-test-step-reader-sts/src/test/kotlin/org/skellig/teststep/reader/sts/StsFileParserTest.kt
+++ skellig-test-step-reader-sts/src/test/kotlin/org/skellig/teststep/reader/sts/StsFileParserTest.kt
@@ -3,7 +3,6 @@ package org.skellig.teststep.reader.sts
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.function.Executable
 import java.net.URISyntaxException
 import java.nio.file.Paths
 
@@ -24,35 +23,35 @@ class StsFileParserTest {
         val firstTestStep = testSteps[0]
         val secondTestStep = testSteps[1]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("Simple test step", firstTestStep["name"]) },
-                Executable { Assertions.assertEquals("POST", firstTestStep["method"]) },
-                Executable { Assertions.assertEquals("\${baseUrl}/a/b/c", firstTestStep["url"]) },
-                Executable { Assertions.assertEquals("\"v 1 2 3\"", getValueFromMap(firstTestStep, "payload", "json", "value")) },
-                Executable { Assertions.assertEquals("\"go\"", getValueFromMap(firstTestStep, "payload", "json", "command")) }
+                { Assertions.assertEquals("Simple test step", firstTestStep["name"]) },
+                { Assertions.assertEquals("POST", firstTestStep["method"]) },
+                { Assertions.assertEquals("\${baseUrl}/a/b/c", firstTestStep["url"]) },
+                { Assertions.assertEquals("\"v 1 2 3\"", getValueFromMap(firstTestStep, "payload", "json", "value")) },
+                { Assertions.assertEquals("\"go\"", getValueFromMap(firstTestStep, "payload", "json", "command")) }
         )
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("Send \\d{1} message (.*) from csv \\(test\\)", secondTestStep["name"]) },
-                Executable { Assertions.assertEquals("POST", secondTestStep["method"]) },
-                Executable { Assertions.assertEquals("/a/b/c", secondTestStep["url"]) },
-                Executable { Assertions.assertEquals("\${user}", getValueFromMap(secondTestStep, "auth", "username")) },
-                Executable { Assertions.assertEquals("\${password}", getValueFromMap(secondTestStep, "auth", "password")) },
-                Executable {
+                { Assertions.assertEquals("Send \\d{1} message (.*) from csv \\(test\\)", secondTestStep["name"]) },
+                { Assertions.assertEquals("POST", secondTestStep["method"]) },
+                { Assertions.assertEquals("/a/b/c", secondTestStep["url"]) },
+                { Assertions.assertEquals("\${user}", getValueFromMap(secondTestStep, "auth", "username")) },
+                { Assertions.assertEquals("\${password}", getValueFromMap(secondTestStep, "auth", "password")) },
+                {
                     Assertions.assertEquals("/resources/templates/msg_get(id).ftl",
                             getValueFromMap(secondTestStep, "body", "template", "file"))
                 },
-                Executable {
+                {
                     Assertions.assertEquals("/resources/data/test1.csv",
                             getValueFromMap(secondTestStep, "body", "template", "csv", "file"))
                 },
-                Executable {
+                {
                     Assertions.assertEquals("TABLE",
                             getValueFromMap(secondTestStep, "body", "template", "csv", "convert"))
                 },
-                Executable {
+                {
                     Assertions.assertEquals("test data with valid values",
                             getValueFromMap(secondTestStep, "body", "template", "csv", "row", "name"))
                 },
-                Executable {
+                {
                     Assertions.assertEquals("1",
                             getValueFromMap(secondTestStep, "body", "template", "csv", "row", "group"))
                 }
@@ -69,8 +68,8 @@ class StsFileParserTest {
 
         val firstTestStep = testSteps[0]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("Run command (.*)", firstTestStep["name"]) },
-                Executable {
+                { Assertions.assertEquals("Run command (.*)", firstTestStep["name"]) },
+                {
                     Assertions.assertEquals(
                             """{
           command: $1
@@ -91,9 +90,9 @@ class StsFileParserTest {
 
         val firstTestStep = testSteps[0]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("Validate response", firstTestStep["name"]) },
-                Executable { Assertions.assertEquals("T1", getValueFromMap(firstTestStep, "validate", "fromTest")) },
-                Executable {
+                { Assertions.assertEquals("Validate response", firstTestStep["name"]) },
+                { Assertions.assertEquals("T1", getValueFromMap(firstTestStep, "validate", "fromTest")) },
+                {
                     Assertions.assertTrue(
                             (getValueFromMap(firstTestStep, "validate", "contains_expected_values") as List<*>)
                                     .containsAll(listOf(
@@ -102,8 +101,8 @@ class StsFileParserTest {
                                             "contains(go go go)",
                                             "regex(.*get(id).*)")))
                 },
-                Executable { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "has_fields", "f1")) },
-                Executable { Assertions.assertEquals("get(id) and more", getValueFromMap(firstTestStep, "validate", "has_fields", "json_path(f1.f2)")) }
+                { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "has_fields", "f1")) },
+                { Assertions.assertEquals("get(id) and more", getValueFromMap(firstTestStep, "validate", "has_fields", "json_path(f1.f2)")) }
         )
     }
 
@@ -117,14 +116,14 @@ class StsFileParserTest {
 
         val firstTestStep = testSteps[0]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("do something big", firstTestStep["name"]) },
-                Executable { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values") as List<*>).size) },
-                Executable { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values", 0) as Map<*, *>).size) },
-                Executable { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "data", "values", 0, "c1")) },
-                Executable { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "data", "values", 0, "c2")) },
-                Executable { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values", 1) as Map<*, *>).size) },
-                Executable { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "data", "values", 1, "c1")) },
-                Executable { Assertions.assertEquals("v4", getValueFromMap(firstTestStep, "data", "values", 1, "c2")) }
+                { Assertions.assertEquals("do something big", firstTestStep["name"]) },
+                { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values") as List<*>).size) },
+                { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values", 0) as Map<*, *>).size) },
+                { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "data", "values", 0, "c1")) },
+                { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "data", "values", 0, "c2")) },
+                { Assertions.assertEquals(2, (getValueFromMap(firstTestStep, "data", "values", 1) as Map<*, *>).size) },
+                { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "data", "values", 1, "c1")) },
+                { Assertions.assertEquals("v4", getValueFromMap(firstTestStep, "data", "values", 1, "c2")) }
         )
     }
 
@@ -150,14 +149,14 @@ class StsFileParserTest {
 
         val firstTestStep = testSteps[0]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("T 1 2 3", getValueFromMap(firstTestStep, "validate", "from test")) },
-                Executable { Assertions.assertEquals("application/json", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "headers", "content-type")) },  // spaced inside the value must be preserved
-                Executable { Assertions.assertEquals("contains(fail  1 )", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "log", "none_match", 0)) },
-                Executable { Assertions.assertEquals("contains( error)", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "log", "none_match", 1)) },
-                Executable { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "regex(.*f3=(\\\\w+).*)")) },
-                Executable { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "json_path(f1.f3)")) },
-                Executable { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "json_path(f1.f2)")) },
-                Executable { Assertions.assertEquals("200", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "status")) }
+                { Assertions.assertEquals("T 1 2 3", getValueFromMap(firstTestStep, "validate", "from test")) },
+                { Assertions.assertEquals("application/json", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "headers", "content-type")) },  // spaced inside the value must be preserved
+                { Assertions.assertEquals("contains(fail  1 )", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "log", "none_match", 0)) },
+                { Assertions.assertEquals("contains( error)", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "log", "none_match", 1)) },
+                { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "regex(.*f3=(\\\\w+).*)")) },
+                { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "json_path(f1.f3)")) },
+                { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "body", "json_path(f1.f2)")) },
+                { Assertions.assertEquals("200", getValueFromMap(firstTestStep, "validate", "any match", "[srv1, srv2, srv3]", "status")) }
         )
     }
 
@@ -171,16 +170,16 @@ class StsFileParserTest {
 
         val firstTestStep = testSteps[0]
         Assertions.assertAll(
-                Executable { Assertions.assertEquals("3", getValueFromMap(firstTestStep, "validate", "size")) },
-                Executable { Assertions.assertEquals("contains(v1)", getValueFromMap(firstTestStep, "validate", "records", "[0]")) },
-                Executable { Assertions.assertEquals("contains(v2)", getValueFromMap(firstTestStep, "validate", "records", "[1]")) },
-                Executable { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 0)) },
-                Executable { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 1)) },
-                Executable { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 2)) },
-                Executable { Assertions.assertEquals("v5", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c2", "any_match", 0)) },
-                Executable { Assertions.assertEquals("v6", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c2", "any_match", 1)) },
-                Executable { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "all_match", 1, "c1")) },
-                Executable { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "[0]", "c1")) }
+                { Assertions.assertEquals("3", getValueFromMap(firstTestStep, "validate", "size")) },
+                { Assertions.assertEquals("contains(v1)", getValueFromMap(firstTestStep, "validate", "records", "[0]")) },
+                { Assertions.assertEquals("contains(v2)", getValueFromMap(firstTestStep, "validate", "records", "[1]")) },
+                { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 0)) },
+                { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 1)) },
+                { Assertions.assertEquals("v3", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c1", "none_match", 2)) },
+                { Assertions.assertEquals("v5", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c2", "any_match", 0)) },
+                { Assertions.assertEquals("v6", getValueFromMap(firstTestStep, "validate", "all_match", 0, "c2", "any_match", 1)) },
+                { Assertions.assertEquals("v2", getValueFromMap(firstTestStep, "validate", "all_match", 1, "c1")) },
+                { Assertions.assertEquals("v1", getValueFromMap(firstTestStep, "validate", "[0]", "c1")) }
         )
     }
 
