199ca8f61ced1627d07ebeea09a26d31080627b6
Shendor <shendor1988@gmail.com>
2021-01-11 18:15:08 +0000

    Refactoring at NumberValueConverter

--- skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/converter/NumberValueConverterTest.kt
+++ skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/converter/NumberValueConverterTest.kt
@@ -15,31 +15,31 @@ class NumberValueConverterTest {
 
     @Test
     fun testConvertInt() {
-        Assertions.assertEquals(450, numberValueConverter!!.convert("(int)450"))
+        Assertions.assertEquals(450, numberValueConverter!!.convert("int(450)"))
     }
 
     @Test
     fun testConvertLong() {
-        Assertions.assertEquals(450L, numberValueConverter!!.convert("(long) 450"))
+        Assertions.assertEquals(450L, numberValueConverter!!.convert("long(450)"))
     }
 
     @Test
     fun testConvertFloat() {
-        Assertions.assertEquals(450f, numberValueConverter!!.convert("(float) 450.0"))
+        Assertions.assertEquals(450f, numberValueConverter!!.convert("float(450.0)"))
     }
 
     @Test
     fun testConvertFloat2() {
-        Assertions.assertEquals(1.0032f, numberValueConverter!!.convert("(float) 1.0032"))
+        Assertions.assertEquals(1.0032f, numberValueConverter!!.convert("float(1.0032)"))
     }
 
     @Test
     fun testConvertDouble() {
-        Assertions.assertEquals(450.0, numberValueConverter!!.convert("(double) 450.0"))
+        Assertions.assertEquals(450.0, numberValueConverter!!.convert("double(450.0)"))
     }
 
     @Test
     fun testConvertDouble2() {
-        Assertions.assertEquals(0.00001, numberValueConverter!!.convert("(double)0.00001"))
+        Assertions.assertEquals(0.00001, numberValueConverter!!.convert("double(0.00001)"))
     }
 }
