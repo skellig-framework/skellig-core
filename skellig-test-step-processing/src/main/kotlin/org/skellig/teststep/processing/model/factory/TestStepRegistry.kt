bf07ed7a3dd60552b1167626ed6a4689037ab704
Shendor <shendor1988@gmail.com>
2021-08-07 21:30:43 +0100

    Missing method in TestStepRegistry

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/model/factory/TestStepRegistry.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/model/factory/TestStepRegistry.kt
@@ -3,4 +3,6 @@ package org.skellig.teststep.processing.model.factory
 interface TestStepRegistry {
 
     fun getByName(testStepName: String): Map<String, Any?>?
+
+    fun getTestSteps(): Collection<Map<String, Any?>>
 }
