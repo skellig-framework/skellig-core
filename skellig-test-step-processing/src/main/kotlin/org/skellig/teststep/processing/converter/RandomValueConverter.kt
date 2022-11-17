7c467f496e1647390d6ae0982e0d6066552d143f
Shendor <shendor1988@gmail.com>
2022-11-17 21:30:20 +0000

    Updated RandomValueConverter function to use new function executor.

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/RandomValueConverter.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/RandomValueConverter.kt
@@ -4,23 +4,19 @@ import org.skellig.teststep.processing.exception.TestDataConversionException
 import org.skellig.teststep.processing.experiment.FunctionValueProcessor
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.util.regex.Pattern
 import kotlin.random.Random
 
 
-class RandomValueConverter : TestStepValueConverter, FunctionValueProcessor {
+class RandomValueConverter : FunctionValueProcessor {
 
-    companion object {
-        private val NOW_PATTERN = Pattern.compile("rand\\(([\\w.]*)\\s*,\\s*([\\w.]+)\\s*,?\\s*([\\w.]*)\\)")
-    }
 
     private val random = Random(System.currentTimeMillis())
 
     override fun execute(name: String, args: Array<Any?>): Any {
-        if (args.size in 2..3) {
-            val min = args[0]?.toString() ?: ""
-            val max = args[1]?.toString() ?: ""
-            return when (args[3]) {
+        if (args.size >= 2) {
+            val min = if (args[0]?.toString()?.isEmpty() == true) "0" else args[0].toString()
+            val max = args[1]?.toString() ?: "0"
+            return when (if (args.size == 3) args[2]?.toString() ?: "" else "") {
                 "int" -> random.nextInt(min.toInt(), max.toInt())
                 "long" -> random.nextLong(min.toLong(), max.toLong())
                 "double" -> random.nextDouble(min.toDouble(), max.toDouble())
@@ -32,30 +28,10 @@ class RandomValueConverter : TestStepValueConverter, FunctionValueProcessor {
         }
     }
 
-    override fun getFunctionName(): String = "rand"
-
-    override fun convert(value: Any?): Any? =
-        when (value) {
-            is String -> {
-                val matcher = NOW_PATTERN.matcher(value.toString())
-                if (matcher.find()) {
-                    val min = if (matcher.group(1) == "") "0" else matcher.group(1)
-                    val max = matcher.group(2)
-                    when (matcher.group(3)) {
-                        "int" -> random.nextInt(min.toInt(), max.toInt())
-                        "long" -> random.nextLong(min.toLong(), max.toLong())
-                        "double" -> random.nextDouble(min.toDouble(), max.toDouble())
-                        "bigDecimal" -> randomBigDecimal(min.toBigDecimal(), max.toBigDecimal())
-                        else -> random.nextLong(min.toLong(), max.toLong())
-                    }
-
-                } else value
-            }
-            else -> value
-        }
-
     private fun randomBigDecimal(min: BigDecimal, max: BigDecimal): BigDecimal {
         val randomBigDecimal = min.add(BigDecimal(random.nextDouble()).multiply(max.subtract(min)))
         return randomBigDecimal.setScale(0, RoundingMode.HALF_UP)
     }
+
+    override fun getFunctionName(): String = "rand"
 }
