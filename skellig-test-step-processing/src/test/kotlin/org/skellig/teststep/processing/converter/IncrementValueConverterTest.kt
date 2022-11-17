800d8027d22dbdff318a0792e414793fa23561d7
Shendor <shendor1988@gmail.com>
2022-11-17 21:29:56 +0000

    Updated IncrementValueConverter function to use new function executor.

--- skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/converter/IncrementValueConverterTest.kt
+++ skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/converter/IncrementValueConverterTest.kt
@@ -23,7 +23,7 @@ class IncrementValueConverterTest {
     @Test
     @DisplayName("Increment value first time Then verify it returned default value with replaced zeros")
     fun testIncrementSimpleValueFirstTime() {
-        val result = converter!!.convert("inc(id,10)")
+        val result = converter!!.execute("inc", arrayOf("id","10"))
 
         Assertions.assertEquals("0000000001", result)
     }
@@ -31,21 +31,20 @@ class IncrementValueConverterTest {
     @Test
     @DisplayName("Increment value second time Then verify it returned incremented value by 1")
     fun testIncrementSimpleValueSecondTime() {
-        val value = "inc(id,10)"
+        val args: Array<Any?> = arrayOf("id", "10")
+        converter!!.execute("inc", args)
 
-        converter!!.convert(value)
+        val result = converter!!.execute("inc", args)
 
-        val result = converter!!.convert(value)
         Assertions.assertEquals("0000000002", result)
     }
 
     @Test
     @DisplayName("Increment value several times Then verify it returned correct incremented value")
     fun testIncrementSimpleValueSeveralTimes() {
-        val value = "inc(id,3)"
         var result: Any? = null
         for (i in 0..9) {
-            result = converter!!.convert(value)
+            result = converter!!.execute("inc", arrayOf("id","3"))
         }
         Assertions.assertEquals("010", result)
     }
@@ -53,10 +52,9 @@ class IncrementValueConverterTest {
     @Test
     @DisplayName("Increment value more times than defined in parameter Then verify it does not overflow")
     fun testIncrementValueMoreThanLengthOfRegex() {
-        val value = "inc(id,1)"
         var result: Any? = null
         for (i in 0..9) {
-            result = converter!!.convert(value)
+            result = converter!!.execute("inc", arrayOf("id","1"))
         }
         Assertions.assertEquals("9", result)
     }
@@ -64,12 +62,9 @@ class IncrementValueConverterTest {
     @Test
     @DisplayName("Increment value with different limits several times Then verify it does not overflow and increment where possible")
     fun testIncrementValueWithDifferentLimits() {
-        val value = "inc(id ,4)"
-        val valueWithId = "inc(id)"
+        converter!!.execute("inc", arrayOf("id","4"))
 
-        converter!!.convert(value)
-
-        val result = converter!!.convert(valueWithId)
+        val result = converter!!.execute("inc", arrayOf("id"))
 
         Assertions.assertEquals("0002", result)
     }
@@ -77,17 +72,14 @@ class IncrementValueConverterTest {
     @Test
     @DisplayName("Increment different values Then verify it takes correct values from file and increment them")
     fun testIncrementDifferentValues() {
-        val value1 = "inc(id, 5)"
-        val value2 = "inc(3)"
-        val value3 = "inc()"
         var result1: Any? = null
         var result2: Any? = null
         var result3: Any? = null
         for (i in 0..4) {
             val converter = IncrementValueConverter()
-            result1 = converter.convert(value1)
-            result2 = converter.convert(value2)
-            result3 = converter.convert(value3)
+            result1 = converter.execute("inc", arrayOf("id","5"))
+            result2 = converter.execute("inc", arrayOf("3"))
+            result3 = converter.execute("inc", emptyArray())
         }
 
         Assertions.assertEquals("00005", result1)
