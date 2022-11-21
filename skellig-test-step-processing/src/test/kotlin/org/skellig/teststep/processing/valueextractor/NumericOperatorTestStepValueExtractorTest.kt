2b41c7e4f024e31e13b154ea3fc0b512ea5fd474
Shendor <shendor1988@gmail.com>
2022-11-21 18:12:16 +0000

    Updated numeric operation extractor

--- skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/valueextractor/NumericOperatorTestStepValueExtractorTest.kt
+++ skellig-test-step-processing/src/test/kotlin/org/skellig/teststep/processing/valueextractor/NumericOperatorTestStepValueExtractorTest.kt
@@ -13,66 +13,66 @@ class NumericOperatorTestStepValueExtractorTest {
     fun testAdd() {
         val extractor = PlusOperatorTestStepValueExtractor()
 
-        assertEquals(5, extractor.extract("2", "3"))
-        assertEquals(5, extractor.extract(2, "3"))
-        assertEquals(5.toShort(), extractor.extract(2.toShort(), "3"))
-        assertEquals(5.toByte(), extractor.extract(2.toByte(), "3"))
-        assertEquals(5.toLong(), extractor.extract(2.toLong(), "3"))
-        assertEquals(5.toFloat(), extractor.extract(2.toFloat(), "3"))
-        assertEquals((5.8).toFloat(), extractor.extract((2.1).toFloat(), "3.7"))
-        assertEquals(5.toDouble(), extractor.extract(2.toDouble(), "3"))
-        assertEquals(5.toBigDecimal(), extractor.extract(2.toBigDecimal(), "3"))
+        assertEquals(5, extractor.extractFrom("plus", "2", arrayOf("3")))
+        assertEquals(5, extractor.extractFrom("plus", 2, arrayOf("3")))
+        assertEquals(5.toShort(), extractor.extractFrom("plus", 2.toShort(), arrayOf("3")))
+        assertEquals(5.toByte(), extractor.extractFrom("plus", 2.toByte(), arrayOf("3")))
+        assertEquals(5.toLong(), extractor.extractFrom("plus", 2.toLong(), arrayOf("3")))
+        assertEquals(5.toFloat(), extractor.extractFrom("plus", 2.toFloat(), arrayOf("3")))
+        assertEquals((5.8).toFloat(), extractor.extractFrom("plus", (2.1).toFloat(), arrayOf("3.7")))
+        assertEquals(5.toDouble(), extractor.extractFrom("plus", 2.toDouble(), arrayOf("3")))
+        assertEquals(5.toBigDecimal(), extractor.extractFrom("plus", 2.toBigDecimal(), arrayOf("3")))
     }
 
     @Test
     fun testTimes() {
         val extractor = TimesOperatorTestStepValueExtractor()
 
-        assertEquals(6, extractor.extract("2", "3"))
-        assertEquals(6, extractor.extract(2, "3"))
-        assertEquals(6.toShort(), extractor.extract(2.toShort(), "3"))
-        assertEquals(6.toByte(), extractor.extract(2.toByte(), "3"))
-        assertEquals(6.toLong(), extractor.extract(2.toLong(), "3"))
-        assertEquals(6.toFloat(), extractor.extract(2.toFloat(), "3"))
-        assertEquals(6.toDouble(), extractor.extract(2.toDouble(), "3"))
-        assertEquals(6.toBigDecimal(), extractor.extract(2.toBigDecimal(), "3"))
+        assertEquals(6, extractor.extractFrom("times", "2", arrayOf("3")))
+        assertEquals(6, extractor.extractFrom("times", 2, arrayOf("3")))
+        assertEquals(6.toShort(), extractor.extractFrom("times", 2.toShort(), arrayOf("3")))
+        assertEquals(6.toByte(), extractor.extractFrom("times", 2.toByte(), arrayOf("3")))
+        assertEquals(6.toLong(), extractor.extractFrom("times", 2.toLong(), arrayOf("3")))
+        assertEquals(6.toFloat(), extractor.extractFrom("times", 2.toFloat(), arrayOf("3")))
+        assertEquals(6.toDouble(), extractor.extractFrom("times", 2.toDouble(), arrayOf("3")))
+        assertEquals(6.toBigDecimal(), extractor.extractFrom("times", 2.toBigDecimal(), arrayOf("3")))
     }
 
     @Test
     fun testDiv() {
         val extractor = DivOperatorTestStepValueExtractor()
 
-        assertEquals(1, extractor.extract("3", "2"))
-        assertEquals(1, extractor.extract(3, "2"))
-        assertEquals(1.toShort(), extractor.extract(3.toShort(), "2"))
-        assertEquals(1.toByte(), extractor.extract(3.toByte(), "2"))
-        assertEquals(1.toLong(), extractor.extract(3.toLong(), "2"))
-        assertEquals(1.5.toFloat(), extractor.extract(3.toFloat(), "2"))
-        assertEquals(1.5, extractor.extract(3.toDouble(), "2"))
-        assertEquals(1.toBigDecimal(), extractor.extract(3.toBigDecimal(), "2"))
+        assertEquals(1, extractor.extractFrom("div", "3", arrayOf("2")))
+        assertEquals(1, extractor.extractFrom("div", 3, arrayOf("2")))
+        assertEquals(1.toShort(), extractor.extractFrom("div", 3.toShort(), arrayOf("2")))
+        assertEquals(1.toByte(), extractor.extractFrom("div", 3.toByte(), arrayOf("2")))
+        assertEquals(1.toLong(), extractor.extractFrom("div", 3.toLong(), arrayOf("2")))
+        assertEquals(1.5.toFloat(), extractor.extractFrom("div", 3.toFloat(), arrayOf("2")))
+        assertEquals(1.5, extractor.extractFrom("div", 3.toDouble(), arrayOf("2")))
+        assertEquals(2.toBigDecimal(), extractor.extractFrom("div", 3.toBigDecimal(), arrayOf("2")))
     }
 
     @Test
     fun testMinus() {
         val extractor = MinusOperatorTestStepValueExtractor()
 
-        assertEquals(-1, extractor.extract("2", "3"))
-        assertEquals(-1, extractor.extract(2, "3"))
-        assertEquals((-1).toShort(), extractor.extract(2.toShort(), "3"))
-        assertEquals((-1).toByte(), extractor.extract(2.toByte(), "3"))
-        assertEquals((-1).toLong(), extractor.extract(2.toLong(), "3"))
-        assertEquals((-1).toFloat(), extractor.extract(2.toFloat(), "3"))
-        assertEquals((-1).toDouble(), extractor.extract(2.toDouble(), "3"))
-        assertEquals((-1).toBigDecimal(), extractor.extract(2.toBigDecimal(), "3"))
-        assertEquals((-1.8).toBigDecimal(), extractor.extract(2.toBigDecimal(), "3.8"))
+        assertEquals(-1, extractor.extractFrom("minus", "2", arrayOf("3")))
+        assertEquals(-1, extractor.extractFrom("minus", 2, arrayOf("3")))
+        assertEquals((-1).toShort(), extractor.extractFrom("minus", 2.toShort(), arrayOf("3")))
+        assertEquals((-1).toByte(), extractor.extractFrom("minus", 2.toByte(), arrayOf("3")))
+        assertEquals((-1).toLong(), extractor.extractFrom("minus", 2.toLong(), arrayOf("3")))
+        assertEquals((-1).toFloat(), extractor.extractFrom("minus", 2.toFloat(), arrayOf("3")))
+        assertEquals((-1).toDouble(), extractor.extractFrom("minus", 2.toDouble(), arrayOf("3")))
+        assertEquals((-1).toBigDecimal(), extractor.extractFrom("minus", 2.toBigDecimal(), arrayOf("3")))
+        assertEquals((-1.8).toBigDecimal(), extractor.extractFrom("minus", 2.toBigDecimal(), arrayOf("3.8")))
     }
 
     @Test
     fun testWitWrongType() {
-        assertThrows<NumberFormatException> { MinusOperatorTestStepValueExtractor().extract(2.toBigDecimal(), "gg") }
-        assertThrows<ValueExtractionException> { PlusOperatorTestStepValueExtractor().extract(2, "gg") }
-        assertThrows<ValueExtractionException> { MinusOperatorTestStepValueExtractor().extract(2, "gg") }
-        assertThrows<ValueExtractionException> { DivOperatorTestStepValueExtractor().extract(2, "gg") }
-        assertThrows<ValueExtractionException> { TimesOperatorTestStepValueExtractor().extract(2, "gg") }
+        assertThrows<NumberFormatException> { MinusOperatorTestStepValueExtractor().extractFrom("add", 2.toBigDecimal(), arrayOf("gg")) }
+        assertThrows<ValueExtractionException> { PlusOperatorTestStepValueExtractor().extractFrom("plus", 2, arrayOf("gg")) }
+        assertThrows<ValueExtractionException> { MinusOperatorTestStepValueExtractor().extractFrom("minus", 2, arrayOf("gg")) }
+        assertThrows<ValueExtractionException> { DivOperatorTestStepValueExtractor().extractFrom("div", 2, arrayOf("gg")) }
+        assertThrows<ValueExtractionException> { TimesOperatorTestStepValueExtractor().extractFrom("times", 2, arrayOf("gg")) }
     }
 }
