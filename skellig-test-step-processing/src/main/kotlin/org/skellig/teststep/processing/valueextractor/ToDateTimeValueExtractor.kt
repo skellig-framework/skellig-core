ff17ae9eb1df3a82913ade1bb87c8b40be954a81
Shendor <shendor1988@gmail.com>
2022-11-21 20:25:19 +0000

    Updated toDate extractor

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/valueextractor/ToDateTimeValueExtractor.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/valueextractor/ToDateTimeValueExtractor.kt
@@ -11,10 +11,9 @@ import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalQuery
 import java.util.regex.Pattern
 
-abstract class BaseToDateTimeValueExtractor : TestStepValueExtractor, ValueExtractor {
+abstract class BaseToDateTimeValueExtractor : ValueExtractor {
 
     companion object {
-        private val SPLIT_PATTERN = Pattern.compile(",")
         internal val UTC = ZoneId.of("UTC")
     }
 
@@ -37,27 +36,6 @@ abstract class BaseToDateTimeValueExtractor : TestStepValueExtractor, ValueExtra
             }
         } else throw TestDataConversionException("Function `${getExtractFunctionName()}` can only accept 1 or 2 String arguments. Found ${args.size}")
     }
-
-    override fun extract(value: Any?, extractionParameter: String?): Any? =
-        when (value) {
-            is String -> {
-                var pattern: String? = null
-                var timezone: String? = null
-                val split = SPLIT_PATTERN.split(extractionParameter ?: "")
-                if (split.isNotEmpty()) {
-                    pattern = split[0].trim()
-                    if (split.size > 1) {
-                        timezone = split[1].trim()
-                    }
-                }
-                pattern?.let {
-                    val dateTimeFormatter = DateTimeFormatter.ofPattern(pattern)
-                        .withZone(timezone?.let { ZoneId.of(timezone) } ?: UTC)
-                    parseDate(value, dateTimeFormatter, getTemporalQuery())
-                } ?: throw ValueExtractionException("Date/Time pattern is mandatory for '${getExtractFunctionName()}' function")
-            }
-            else -> value
-        }
 
     protected abstract fun getTemporalQuery(): TemporalQuery<*>;
 
