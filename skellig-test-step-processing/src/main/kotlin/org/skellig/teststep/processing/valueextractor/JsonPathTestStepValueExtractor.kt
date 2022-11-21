c18ad16e2ae8f7223ced3bab0dcfba417a1a9734
Shendor <shendor1988@gmail.com>
2022-11-21 17:58:34 +0000

    Updated jsonPath extractor

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/valueextractor/JsonPathTestStepValueExtractor.kt
+++ skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/valueextractor/JsonPathTestStepValueExtractor.kt
@@ -6,13 +6,9 @@ import org.skellig.teststep.processing.exception.ValueExtractionException
 import org.skellig.teststep.processing.experiment.ValueExtractor
 import java.util.regex.Pattern
 
-class JsonPathTestStepValueExtractor : TestStepValueExtractor, ValueExtractor {
+class JsonPathTestStepValueExtractor : ValueExtractor {
 
-    companion object {
-        private val PARAM_SPLIT_PATTERN = Pattern.compile(",")
-    }
-
-    override fun extractFrom(name: String, value: Any?, args: Array<Any?>): Any {
+    override fun extractFrom(name: String, value: Any?, args: Array<Any?>): Any? {
         if (args.isNotEmpty()) {
             return value?.let {
                 try {
@@ -20,32 +16,12 @@ class JsonPathTestStepValueExtractor : TestStepValueExtractor, ValueExtractor {
                     json.getString(args[0]?.toString())
                 } catch (ex: Exception) {
                     if (args.size == 1 || (args.size == 2 && args[1]?.toString()?.trim() != "true")) {
-                        throw ValueExtractionException(
-                            "Failed to extract jsonPath '$args' from value '$value'. " +
-                                    "Reason ${ex.message}"
-                        )
+                        throw ValueExtractionException("Failed to extract jsonPath '${args[0]}' from value '$value'. Reason ${ex.message}")
                     } else null
                 }
-            } ?: throw ValueExtractionException("Failed to extract jsonPath from null value")
+            }
         } else {
             throw TestDataConversionException("Function `jsonPath` can only accept 1 or 2 arguments. Found ${args.size}")
-        }
-    }
-
-    override fun extract(value: Any?, extractionParameter: String?): Any? {
-        return value?.let {
-            val params = PARAM_SPLIT_PATTERN.split(extractionParameter ?: "")
-            try {
-                val json = JsonPath.from(it as String)
-                json.getString(params[0])
-            } catch (ex: Exception) {
-                if (params.size == 1 || (params.size == 2 && params[1].trim() != "true")) {
-                    throw ValueExtractionException(
-                        "Failed to extract jsonPath '$extractionParameter' from value '$value'. " +
-                                "Reason ${ex.message}"
-                    )
-                } else null
-            }
         }
     }
 
