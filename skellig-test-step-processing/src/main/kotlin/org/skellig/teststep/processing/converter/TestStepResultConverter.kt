959808b2ff54f8a264e908b755e6b4617bef33d8
Shendor <shendor1988@gmail.com>
2022-11-24 17:46:29 +0000

    Removed unused result converter.

--- skellig-test-step-processing/src/main/kotlin/org/skellig/teststep/processing/converter/TestStepResultConverter.kt
+++
@@ -1,8 +0,0 @@
-package org.skellig.teststep.processing.converter
-
-interface TestStepResultConverter {
-
-    fun convert(convertFunction: String, result: Any?): Any?
-
-    fun getConvertFunctionName(): String
-}
