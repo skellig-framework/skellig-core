a8b97665fce412f99e6214b29604e77fe420e615
shendor <shendor1988@gmail.com>
2023-07-15 10:52:20 +0100

    Added test for Test Scenario state

--- skellig-junit-runner/src/test/kotlin/org/skellig/teststep/processing/state/DefaultTestScenarioStateTest.kt
+++ skellig-junit-runner/src/test/kotlin/org/skellig/teststep/processing/state/DefaultTestScenarioStateTest.kt
@@ -13,4 +13,31 @@ class DefaultTestScenarioStateTest {
 
         assertEquals("value1", scenarioState.get("key1"))
     }
+
+    @Test
+    fun testRemove() {
+        scenarioState.set("key1", "value1")
+        scenarioState.remove("key1")
+
+        assertNull(scenarioState.get("key1"))
+    }
+
+    @Test
+    fun testClean() {
+        scenarioState.set("key1", "value1")
+        scenarioState.clean()
+
+        assertNull(scenarioState.get("key1"))
+    }
+
+    @Test
+    fun testIterator() {
+        scenarioState.set("key1", "value1")
+        scenarioState.set("key2", "value2")
+        val iterator = scenarioState.iterator()
+
+
+        assertEquals("value1", iterator.next())
+        assertEquals("value2", iterator.next())
+    }
 }
