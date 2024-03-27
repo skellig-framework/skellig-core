562dcba1a32b58b90eb5f1013bef59a8a6c74e4e
shendor <shendor1988@gmail.com>
2024-03-27 11:38:02 +0000

    Fixed flaky test

--- skellig-task/src/test/kotlin/org/skellig/task/async/AsyncTaskUtilsTest.kt
+++ skellig-task/src/test/kotlin/org/skellig/task/async/AsyncTaskUtilsTest.kt
@@ -1,16 +1,14 @@
 package org.skellig.task.async
 
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.skellig.task.async.AsyncTaskUtils.Companion.runCallableAsync
 import org.skellig.task.async.AsyncTaskUtils.Companion.runTaskAsync
 import org.skellig.task.async.AsyncTaskUtils.Companion.runTasksAsyncAndWait
 import org.skellig.task.async.AsyncTaskUtils.Companion.shutdown
 import org.skellig.task.exception.TaskRunException
 import java.util.*
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.ExecutionException
-import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 
 class AsyncTaskUtilsTest {
 
@@ -35,7 +33,7 @@ class AsyncTaskUtilsTest {
                 Thread.sleep(50)
                 RESPONSE
             }
-            Assertions.assertEquals(RESPONSE, asyncResult[500, TimeUnit.MILLISECONDS])
+            assertEquals(RESPONSE, asyncResult[500, TimeUnit.MILLISECONDS])
         }
 
         @Test
@@ -54,19 +52,31 @@ class AsyncTaskUtilsTest {
             }, { obj: String? -> Objects.nonNull(obj) }, 10, 1000)
 
             countDownLatch.await(1000, TimeUnit.MILLISECONDS)
-            Assertions.assertEquals(0, countDownLatch.count)
-            Assertions.assertEquals(RESPONSE, asyncResult[10, TimeUnit.MILLISECONDS])
+            assertEquals(0, countDownLatch.count)
+            assertEquals(RESPONSE, asyncResult[10, TimeUnit.MILLISECONDS])
         }
 
         @Test
-        @DisplayName("When getting result from async operation not received within timeout")
+        @DisplayName("When getting result from async operation And inner exception is thrown")
         fun testAsyncTaskWhenThrowException() {
             val asyncResult: Future<String> = runCallableAsync {
                 Thread.sleep(100)
                 throw TaskRunException("oops")
             }
 
-            Assertions.assertThrows(ExecutionException::class.java) { asyncResult[300, TimeUnit.MILLISECONDS] }
+            val ex = Assertions.assertThrows(ExecutionException::class.java) { asyncResult[1000, TimeUnit.MILLISECONDS] }
+            assertEquals("org.skellig.task.exception.TaskRunException: oops", ex.message)
+        }
+
+        @Test
+        @DisplayName("When getting result from async operation not received within timeout")
+        fun testAsyncTaskWhenNotReceivedWithinTimeout() {
+            val asyncResult: Future<String> = runCallableAsync {
+                Thread.sleep(200)
+                "result"
+            }
+
+            Assertions.assertThrows(TimeoutException::class.java) { asyncResult[10, TimeUnit.MILLISECONDS] }
         }
 
         @Nested
@@ -77,18 +87,19 @@ class AsyncTaskUtilsTest {
             fun testAsyncTaskAndGetResult() {
                 val asyncResult = runTasksAsyncAndWait(mapOf(Pair("t1", { "r1" })), { true }, 0, 0, 500)
 
-                Assertions.assertEquals("r1", asyncResult["t1"])
+                assertEquals("r1", asyncResult["t1"])
             }
 
             @Test
             @DisplayName("And condition not satisfied Then check latest non-null result returned")
             fun testAsyncTaskWhenConditionNotSatisfied() {
-                var counter = 0;
+                var counter = 0
                 val asyncResult = runTasksAsyncAndWait(
-                        mapOf(Pair("t1", { if(counter++ == 1) "r1" else null })),
-                        { false }, 1, 5, 500)
+                    mapOf(Pair("t1") { if (counter++ == 1) "r1" else null }),
+                    { false }, 1, 5, 500
+                )
 
-                Assertions.assertEquals("r1", asyncResult["t1"])
+                assertEquals("r1", asyncResult["t1"])
             }
         }
     }
@@ -100,10 +111,10 @@ class AsyncTaskUtilsTest {
         @Test
         @DisplayName("When many tasks And results received within a timeout")
         fun testAsyncTasks() {
-            val asyncResult = runTasksAsyncAndWait(createMapOfTasks(150, 50), { it["t1"] != null && it["t2"] != null  }, 0, 0, 500)
+            val asyncResult = runTasksAsyncAndWait(createMapOfTasks(150, 50), { it["t1"] != null && it["t2"] != null }, 0, 0, 500)
 
-            Assertions.assertEquals("r1", asyncResult["t1"])
-            Assertions.assertEquals("r2", asyncResult["t2"])
+            assertEquals("r1", asyncResult["t1"])
+            assertEquals("r2", asyncResult["t2"])
         }
 
         @Test
@@ -112,7 +123,7 @@ class AsyncTaskUtilsTest {
             val asyncResult = runTasksAsyncAndWait(createMapOfTasks(3000, 50), { it["t1"] != null }, 0, 0, 200)
 
             Assertions.assertNull(asyncResult["t1"])
-            Assertions.assertEquals("r2", asyncResult["t2"])
+            assertEquals("r2", asyncResult["t2"])
         }
 
         @Test
@@ -120,8 +131,8 @@ class AsyncTaskUtilsTest {
         fun testAsyncTasksWhenTimeoutZero() {
             val asyncResult = runTasksAsyncAndWait(createMapOfTasks(120, 20), { it["t1"] != null && it["t2"] != null }, 0, 0, 0)
 
-            Assertions.assertEquals("r1", asyncResult["t1"])
-            Assertions.assertEquals("r2", asyncResult["t2"])
+            assertEquals("r1", asyncResult["t1"])
+            assertEquals("r2", asyncResult["t2"])
         }
 
         @Test
@@ -129,8 +140,8 @@ class AsyncTaskUtilsTest {
         fun testAsyncTasksWithRetries() {
             val asyncResult = runTasksAsyncAndWait(createMapOfTasks(150, 50, 3), { it["t1"] != null }, 0, 3, 0)
 
-            Assertions.assertEquals("r1", asyncResult["t1"])
-            Assertions.assertEquals("r2", asyncResult["t2"])
+            assertEquals("r1", asyncResult["t1"])
+            assertEquals("r2", asyncResult["t2"])
         }
 
         @Test
@@ -139,26 +150,27 @@ class AsyncTaskUtilsTest {
             val asyncResult = runTasksAsyncAndWait(createMapOfTasks(150, 50, 3), { it["t1"] != null }, 0, 2, 0)
 
             Assertions.assertNull(asyncResult["t1"])
-            Assertions.assertEquals("r2", asyncResult["t2"])
+            assertEquals("r2", asyncResult["t2"])
         }
 
         private fun createMapOfTasks(taskOneDelay: Long, taskTwoDelay: Long, taskOneMaxRetry: Int = 0): Map<String, () -> String?> {
             var inc = 0
-            return mapOf(Pair("t1",
-                    {
-                        if (taskOneMaxRetry > 0) {
-                            // this case is triggered when testing a task with retries
-                            Thread.sleep(10)
-                            if (++inc >= taskOneMaxRetry) "r1" else null
-                        } else {
-                            Thread.sleep(taskOneDelay)
-                            "r1"
-                        }
-                    }),
-                    Pair("t2", {
-                        Thread.sleep(taskTwoDelay)
-                        "r2"
-                    })
+            return mapOf(Pair(
+                "t1"
+            ) {
+                if (taskOneMaxRetry > 0) {
+                    // this case is triggered when testing a task with retries
+                    Thread.sleep(10)
+                    if (++inc >= taskOneMaxRetry) "r1" else null
+                } else {
+                    Thread.sleep(taskOneDelay)
+                    "r1"
+                }
+            },
+                Pair("t2") {
+                    Thread.sleep(taskTwoDelay)
+                    "r2"
+                }
             )
         }
     }
